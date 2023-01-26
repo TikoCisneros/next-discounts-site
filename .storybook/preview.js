27908b64bb7adae6f909ff16f36142cc8f5db60d
Luis Cisneros <tiko.cisneros@gmail.com>
2023-01-25 20:44:03 -0500

    build: install  @storybook/addon-postcss and improve styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
-import '../styles/globals.css'
+// more info see https://dev.to/opensauced/set-up-a-nextjs-project-with-typescript-tailwind-and-storybook-3j1k
+import '../styles/reset.scss';
+import '../styles/globals.scss';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
